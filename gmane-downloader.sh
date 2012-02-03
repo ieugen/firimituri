b2bb42464641c8b9524bc27cd699d6c5db82d7e5
Ioan Eugen Stan <stan.ieugen@gmail.com>
2012-02-03 23:55:03 +0200

    Changes provided by qamis@gmail.com
    changelog:
    - autodetect last email
    - post-check th downloaded file for errors generated by php's export,
    and exit if errors detected
    - split the downloaded file into individual emails
    - prettier output

--- gmane-downloader.sh
+++ gmane-downloader.sh
@@ -1,54 +1,76 @@
 #!/bin/bash
-# 
-# This script downloades mailing list archives from GMane. It downloades them in
-# chunks and saves them as mbox files in a directory that's named after the mailing list.
-#
-# Required parameters are: 
-#   - <gmane mailing list name> (e.g. gmane.org.user-groups.rlug.general)
-#   - start 
-#   - end 
 
-if [ -n "$1" ]
-then 
+# Script that uses gmane.org web interface to download a mailing list archive. 
+# The mailing list has to be registered on gmane:)
+# You may search for a mailing list using http://search.gmane.org/
+# Ex: http://dir.gmane.org/search.php?match=.rlug.
+# Tested on 
+# 	gmane.org.user-groups.rlug.general
+# 	gmane.org.user-groups.rlug.offtopic
+
+if [ -n "$1" ]; then 
     MLIST=$1;
 else
-    echo "Usage: $0 <gmane mailing list> [start] [end]\n";
+    echo "Usage: $0 <gmane mailing list> [STEP] [START] [END]";
+	echo;
     exit 1;
 fi
 
-if [ -n "$2" ]
-then
+
+if [ -n "$2" ]; then
     START=$2;
-    echo "Starting at message $START";
+    echo "	download from message $START";
 else
-    START=1
-    echo "WARNING: START not suppied, defaulting to $START";
+    START=1;
+    echo "	START not suppied, defaulting to $START";
 fi
 
-if [ -n "$3" ]
-then
-    END=$3 
-    echo "Downloading $STEP messages at a time";
+#STEP at around 5000 will make the gmame server kill the export script(@see set_time_limit)
+if [ -n "$3" ]; then
+	STEP=$3 
+	echo "	downloading $STEP messages at a time";
 else
-    END=500000
-    echo "Stepping not suppied, defaulting to $STEP";
+	STEP=1500;
+	echo "	STEP not suppied, defaulting to $STEP";
 fi
 
-echo "Begin downlod from list $MLIST messages: $START -> $STEP";
+if [ -n "$4" ]; then
+	END=$4
+	echo "	downloading until message $END";
+else
+	echo "	END not supplied, autodetect from server RSS feed";
+	END=`wget --quiet -O - "http://rss.gmane.org/messages/excerpts/$MLIST" | grep '<item rdf:about="http://permalink.gmane.org' | egrep -o "[0-9]+" | sort | uniq | head -n 1`;
+	
+	#END=500000;
+	echo "	END not suppied, autodetected as $END";
+fi
 
-#set the step to 5000, any larger and we might make the server kill the link
-STEP=5000
+
+echo "Begin download $MLIST, $START -> $END : $STEP";
+
 
 mkdir "$MLIST";
-cd "$MLIST"
+cd "$MLIST";
+
+POST_OP="split";
+
+# perform a for-loop to get all the messages until we reach the limit
 for (( i=$START; i<=$END; i+=$STEP )); do
-       s=$(( $i + $STEP ));
-    wget --output-document="$s" "http://download.gmane.org/$MLIST/$i/$s";
-    FSIZE=`stat -c %s $s`
-    if [[ "$FSIZE" -eq "0" ]]
-    then
-        echo "Done!";
-	rm $s;
-        exit 0;
-    fi
+	s=$(( $i + $STEP ));
+	
+	FNAME=`printf "archive-%07d-%d" $i $s`;
+	
+	printf "%02d%%: " $(( 100*$i / $END ));
+	wget -nv --output-document="$FNAME" "http://download.gmane.org/$MLIST/$i/$s";
+	
+	# perform a basic sanity check. Sometimes the gmame script will end with a fatal error because of php's set_time_limit function
+	INVALID=`tail -n 2 "$FNAME" | grep "Fatal error"`;
+	if [ -n "$INVALID" ]; then
+		echo "invalid download $FNAME. a fatal error was encountered. Please retry with a lower STEP";
+		exit 2;
+	else
+		if [ "$POST_OP" = "split" ]; then
+			csplit -b "%04d" -f "$FNAME-" -k --quiet -z "$FNAME" "/^From news@gmane.org/" "{*}"
+		fi;
+	fi;
 done
